6f71feb239a0079e70d9d1bcdfbbe70598a1b81b
Kham Lao <shwegyimontkyut@gmail.com>
2025-03-27 22:08:19 +0700

    add production env

--- config.py
+++ config.py
@@ -14,6 +14,7 @@ class DevelopmentConfig(Config):
         
 class ProductionConfig(Config):
     DEBUG = True
+    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URI')
     
         
 config = {
